4be4281ed2bfbd9d291f89ed371c829420395c2b
ellestuff. <ellef1566@gmail.com>
2025-06-12 01:02:52 +0100

    portfolio

--- wallpapers/wallpapers.js
+++ wallpapers/wallpapers.js
@@ -1,21 +1,21 @@
 function setWallpaper(val) {
     var img = document.getElementById("trophyroom_img");
-    var a = document.getElementById("trophyroom_a")
+    var a = document.getElementById("trophyroom_a");
 
     var path = "";
 
     switch(val) {
         case 1:
-            path = "images/wallpapers/furry_trophy_room/Furry's Trophy Room - Clean.png"
+            path = "furry_trophy_room/Furry's Trophy Room - Clean.png";
             break;
         case 2:
-            path = "images/wallpapers/furry_trophy_room/Furry's Trophy Room - BG.png"
+            path = "furry_trophy_room/Furry's Trophy Room - BG.png";
             break;
         case 3:
-            path = "images/wallpapers/furry_trophy_room/Furry's Trophy Room - Clean BG.png"
+            path = "furry_trophy_room/Furry's Trophy Room - Clean BG.png";
             break;
         default:
-            path = "images/wallpapers/furry_trophy_room/Furry's Trophy Room.png"
+            path = "furry_trophy_room/Furry's Trophy Room.png";
             break;
     }
 
@@ -27,7 +27,7 @@ function updateWallpaper() {
     var blood = document.getElementById("trophyroom_blood").checked;
     var bg = document.getElementById("trophyroom_bg").checked;
 
-    setWallpaper((blood ? 1 : 0) + (bg ? 2 : 0));
+    setWallpaper((blood ? 0 : 1) + (bg ? 2 : 0));
 }
 
 updateWallpaper()
